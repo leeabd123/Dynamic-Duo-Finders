960a0a04c2114a1d3594d0f8585b3a257f3c7bcc
leeabd123 <90123334+leeabd123@users.noreply.github.com>
2022-11-24 09:40:28 -0600

    Updated the Graph Class

--- src/graph.hpp
+++ src/graph.hpp
@@ -11,7 +11,26 @@ using namespace std;
 
 class Graph {
     public:
-    Graph() {}
+    int V;
+    std::unordered_set<int> *l;
+    Graph(int V) {
+        this->V = V;
+        l = new std::unordered_set<int>[V];
+    }
+    void addEdge(int x, int y) {
+        l[x].insert(y);
+        l[y].insert(x);
+    }
+    void printAdjList() {
+        for (int i = 0; i < V; i++) {
+            std::cout << "Vertex " << i << "->";
+            for (int n : l[i]) {
+                std::cout << n << ",";
+            }
+            std::cout << std::endl;
+        }
+    } 
+
 
     // returns the ids of nodes visited started from input node id in a bfs
     vector<int> BFS(int start_id);
@@ -19,6 +38,6 @@ class Graph {
 
     private:
     vector<Node> nodes_;
-}
+};
 
 #endif
