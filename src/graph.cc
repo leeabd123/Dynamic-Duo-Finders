8b12659b9c27956325b75b07ff71b9188a015195
salsh2 <89149413+salsh2@users.noreply.github.com>
2022-12-13 00:19:00 -0600

    Update graph.cc

--- src/graph.cc
+++ src/graph.cc
@@ -139,9 +139,7 @@ vector<int> Graph::A_Short_Path_Src_to_End() {
               a_path.push_back(nodes_.at(parnode).id);
               parnode = parent[parnode];
         }
-        if (src_node == 0) {
-            a_path.push_back(nodes_.at(0).id);
-        }
+        a_path.push_back(nodes_.at(src_node).id);
         cout << endl;
         break;
     }
@@ -159,9 +157,9 @@ void Graph::Print_Shortest_Paths() {
                cout << " -> " << parnode << " ";
                parnode = parent[parnode];
         }
-        //if (src_node == 0) {
+        if (src_node == 0) {
             cout << "-> " << nodes_.at(0).id << endl;
-        //}
+        }
         cout << endl;
 
     }
@@ -177,9 +175,9 @@ void Graph::Print_A_Shortest_Path() {
                cout << " -> " << nodes_.at(parnode).id << " ";
                parnode = parent[parnode];
         }
-        //if (src_node == 0) {
+        if (src_node == 0) {
             cout << "-> " << nodes_.at(0).id << endl;
-        //}
+        }
         cout << endl;
         break;
 
@@ -216,25 +214,27 @@ vector<int> Graph::Betweenness() {
     // for node in nodes_
     for (Node node: nodes_) {
         // for node in neighbors
-        for (pair<int, double> neighbor: node.neighbors) {
-            if (traveled[node.id][neighbor.first] == 0) { // path not already traveled
+        for (Node node2: nodes_) {
+            if (traveled[node.id][node2.id] == 0) { // path not already traveled
                 /// set this path to traveled (both ways)
-                traveled[node.id][neighbor.first] = 1;
-                traveled[neighbor.first][node.id] = 1;
+                traveled[node.id][node2.id] = 1;
+                traveled[node2.id][node.id] = 1;
                 /// get shortest path between node and neighbor
-                //vector<int> path = Shortest_Path(node.id, neighbor.first);
+                set_start_end(node.id,node2.id);
+                Shortest_Path_Algo();
+                vector<int> path = A_Short_Path_Src_to_End();
+                std::reverse(path.begin(), path.end());
                 /// increment scores for all intermediate nodes in path
-                // for (unsigned int i = 1; i < path.size() - 1; i++) {
-                //     int n = path[i];
-                //     scores[n] = scores[n] + 1;
-                // }
+                for (unsigned int i = 1; i < path.size() - 1; i++) {
+                    int n = path[i];
+                    scores[n] = scores[n] + 1;
+                }
             }
 
         }
     }
     return scores;
 }
-
 
 // ------------- Parsing Stuff ------------------------- //
 std::string file_to_string(const std::string& filename){
